773fdc7cb0bfcee9624aae00456f6e4df2f8bc11
wilsonchenghy <150957380+wilsonchenghy@users.noreply.github.com>
2024-05-14 11:54:46 +0800

    Tuned PID Values and Add Prvent integral windup method

--- PID.py
+++ PID.py
@@ -11,17 +11,17 @@ SETPOINT = 0
 
 INITIAL_HEIGHT = -50
 
+# PID Values that can be tuned (can be tuned by using the stabilization time printed in the terminal)
 KP = 0.9
-KI = 0.15
-KD = 0.32
+KI = 0.2
+KD = 0.4
 
 # Properties of the rocket
 MASS = 1
-MAX_THRUST = 30
+MAX_THRUST = 15
 
 # Properties of environment
 GRAVITY = -9.81
-# GRAVITY = -2000
 
 
 
@@ -48,6 +48,8 @@ class Simulation(object):
         presentTime = time.time()
         prevTime = presentTime
         currentTime = 0
+        counterForStabilization = 0
+        isSteady = False
 
         # Simulation run for simDuration second
         while ((time.time() - presentTime) < self.simDuration):
@@ -65,11 +67,21 @@ class Simulation(object):
                 thrust = self.pid_controller.computeThrust(rocketHeight)
                 self.rocket.enablePhysics(thrust)
 
-                # Print important data (print every 10 TIME_STEP)
-                if (currentTime % 1 == 0):
-                    print(f'Thrust: {thrust}')
+                # Print important data (print every 1 TIME_STEP)
+                if (currentTime % 1 == 0): # can change the numbers to print out data slower
+                    # print(f'Thrust: {thrust}')
                     # self.rocket.printData()
-                    print(" ")
+                    # print(" ")
+
+                    # Print stabilization time
+                    if abs(rocketHeight - SETPOINT) < 2:
+                        counterForStabilization += 1
+                        if (counterForStabilization > 20 and (not isSteady)):
+                            print('Reach stable state')
+                            print(f'Time Taken: {currentTime}')
+                            isSteady = True
+                    else:
+                        counterForStabilization = 0
 
 
                 prevTime = time.time()
@@ -128,6 +140,7 @@ class PID(object):
         self.prevError = 0
         self.derivativeError = 0
         self.integralError = 0
+        self.output = 0
     
     def computeThrust(self, currentPoint):
         self.error = self.setPoint - currentPoint
@@ -139,23 +152,23 @@ class PID(object):
         # print(" ")
         self.prevError = self.error
 
-        self.integralError += self.error * TIME_STEP
-        # print(f'integralError: {self.integralError}')
-
-        # Prevent windup
-        if abs(self.error) < 0.01:
-            self.integralError = 0
-
         # print(f'1: {self.error * self.Kp}')
         # print(f'2: {self.derivativeError * self.Kd}')
         # print(f'3: {self.error * self.Ki}')
         self.output = self.error * self.Kp + self.derivativeError * self.Kd + self.integralError * self.Ki
 
+        # Prevent integral windup
+        if (abs(self.output) >= MAX_THRUST) and ((self.error >= 0 and self.integralError >= 0) or (self.error < 0 and self.integralError < 0)):
+            self.integralError = self.integralError
+        else:
+            self.integralError += self.error * TIME_STEP
+            # print(f'integralError: {self.integralError}')
+
 
         if (self.output > MAX_THRUST):
             self.output = MAX_THRUST
-        # if (self.output < 0):
-        #     self.output = 0
+        if (self.output < 0):
+            self.output = 0
 
         return self.output
 
